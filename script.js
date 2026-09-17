4f8e3bc728e4bdfb8bb0182aa5cc717e9c627f75
Ludwig Kallin <luddekallin52@gmail.com>
2026-09-17 15:59:53 +0200

    småjusteringar samt la till felmeddelande ifall inpu ät tom

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+//deklarera variabler
 let input;
+let completed = 0;
 
 const addBtn = document.querySelector("#addBtn");
 const addToListInput = document.querySelector("#addToListInput");
+const info = document.querySelector("#info");
 
+//event listener till knapp
 addBtn.addEventListener(
     "click",
     addToListFunction
 );
 
+//funktion för lägga till input till lista
 function addToListFunction(){
     input = addToListInput.value;
     const node = document.createElement("li");
@@ -16,4 +21,10 @@ function addToListFunction(){
 
     document.querySelector("#ulList").appendChild(node);
 
+    if (input == ""){
+        info.innerHTML= "Write something to add to list"
+    }
+
 }
+
+//funktion för göra ett element i listan "complete"
