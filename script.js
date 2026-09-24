feffa66450475b36fb7c576c3956719fe23efdb1
Ludwig Kallin <luddekallin52@gmail.com>
2026-09-24 15:07:35 +0200

    Försökt få till att synka listan med array men har inte lyckats

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ const info = document.querySelector("#info");
 const list = document.querySelector("#ulList");
 const amountCompleted = document.querySelector("#amountCompleted");
 
-//trashcan img
-const trashcan = new Image(22,22);
-trashcan.src = "trashcan.png";
-
 
 
 //deklarerar listans array
@@ -24,11 +20,11 @@ addBtn.addEventListener(
 );
 
 //event listener för att kunna använda enterknappen också för att köra funktionen
-addBtn.addEventListener(
-    "keypress",
-    function (e){
-        if (e.key === "Enter"){
-            addToListFunction(e);
+addToListInput.addEventListener(
+    "keydown",
+    function (event){
+        if (event.code === "Enter"){
+            addToListFunction();
         }
     }
 );
@@ -37,50 +33,96 @@ addBtn.addEventListener(
 //funktion för lägga till input till lista
 function addToListFunction(){
     input = addToListInput.value;
+    let completeStatus = false;
 
+    //skriver ut ett meddelande om input är tom
     if (input.trim() === ""){
         info.innerHTML= "Write something to add to list"
         return;
     }
     else{
+        //tar bort meddelandet om man skrivit något i input
         info.innerHTML="";
         let item = document.createElement("li");
         const itemLabel = document.createElement("span");
         itemLabel.innerText = input;
+        //trashcan img
+        const trashcan = new Image(22,22);
+        trashcan.src = "trashcan.png";
     
         item.appendChild(itemLabel);
-        //item.appendChild(trashcan);
+        item.appendChild(trashcan);
         list.appendChild (item);
 
         //lägger till objektet i listan i js
-        listArray.push({item: itemLabel, completeStatus: false});
+            const object = {};
+            object.name = input;
+            object.completeStatus;
+            listArray.push({input, completeStatus});
+
+        //Event listener för att ta bort objektet från listan och array genom att trycka på soptunnan
+            trashcan.addEventListener("click", function(){
+                trashcan.parentElement.remove()
+
+                //räkna om "completed"
+                let completedStyle = document.querySelectorAll(".completedStyle");
+                completed = completedStyle.length
+                amountCompleted.innerHTML = completed + " completed";
+
+                
+
+                // Remove from array
+                let index = listArray.map(t => t.input).indexOf(trashcan.parentElement);
         
+                listArray.splice(index, 1);
+                console.log(index);
+
+                console.log(listArray);
+            });
+        
+        
+        
+
+
         //event listener till varje objekt med funktionen att toggle "completed"
-        item.addEventListener("click", function (){ 
+        itemLabel.addEventListener("click", function (){ 
             if (item.getAttribute("class")=="completedStyle"){
                 item.setAttribute("class","");
-                completeStatus = false;
                 completed--;
+                
+                changeStatus(item, false);
+
+                console.log(listArray);
             }
             else{
                 item.setAttribute("class", "completedStyle");
-                completeStatus = true;
                 completed++;
+                
+                changeStatus(item, true);
+
+                console.log(listArray);
+                
             }
+
+
+            //uppdaterar hur många som är "completed" 
+            amountCompleted.innerHTML = completed + " completed";
             
-           amountCompleted.innerHTML = completed + " completed";
         });
 
-       /* trashcan.addEventListener("click", function(){
-            trashcan.parentElement.remove()
-
-        });*/
-
+        console.log(listArray); //skriver ut array i konsolen för kontroll
+        
+        
     }
 
 
     //clear input
     addToListInput.value = "";
+}
+
+function changeStatus(input, status){
+    let changeIndex = listArray.map(t=>t.input).indexOf(input);
+    listArray[changeIndex].completeStatus = status;
 }
 
 
